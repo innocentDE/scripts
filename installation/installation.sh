9de7606e12d05383da0d4cf0718007037e1c6fff
innocent <vincent.burkhardt.home@gmail.com>
2024-07-31 00:27:20 +0200

    first idea

--- installation/installation.sh
+++ installation/installation.sh
@@ -1,1 +1,26 @@
-#!/usr/bin/bash
+#!/bin/bash
+
+packages=("git" "curl" "fzf" "vim" "htop")
+choice=$(gum choose "Download all packages" "Select packages individually")
+
+if [ "$choice" == "Download all packages" ]; then
+  selected_packages=("${packages[@]}")
+else
+  selected_packages=($(gum choose --no-limit "${packages[@]}"))
+fi
+
+echo "You have selected the following packages:"
+for package in "${selected_packages[@]}"; do
+  echo "- $package"
+done
+
+if gum confirm "Do you want to proceed with downloading these packages?"; then
+  for package in "${selected_packages[@]}"; do
+    echo "Downloading $package..."
+    curl -o "${package}.bin" https://ash-speed.hetzner.com/100MB.bin
+    echo "$package downloaded."
+  done
+  echo "All selected packages have been downloaded."
+else
+  echo "Aborted."
+fi
